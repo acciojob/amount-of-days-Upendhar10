7042bfc4da23a0e5c4df89bcdb482348f8f6c9a4
N.Upendhar <80301044+Upendhar10@users.noreply.github.com>
2025-06-13 16:07:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function daysOfAYear(year){
+	// if(year < 1582){
+	// 	return "Invalid Input";
+	// }
+
+	if((year % 400 === 0 && year % 100 != 0) ||(year % 4 === 0)){
+		return 366;
+	}
+	return 365;
+}
